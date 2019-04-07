01675a71595e987dc14775327dcca82897665390
Altoor <sebastian@pda.dk>
2019-04-08 00:00:21 +0200

    small update

--- src/main/java/midianalyser/model/midiLoader.java
+++ src/main/java/midianalyser/model/midiLoader.java
@@ -1,5 +1,16 @@
 package midianalyser.model;
 
+import java.io.File;
+
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+//TODO checkout https://stackoverflow.com/questions/3850688/reading-midi-files-in-java
+
 public class midiLoader{
 
 }
